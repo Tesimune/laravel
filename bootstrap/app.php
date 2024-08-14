9ecbfad85a4c1c57dd2bc61598d37563d8adf0a6
Teslim Abdulwahab <tesimune@gmail.com>
2024-08-14 06:54:27 +0100

    Updated bootstap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +27,55 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        
+        $exceptions->render(function (AuthenticationException $e, Request $request) {
+            if ($request->expectsJson()) {
+                $response = [
+                    "status" => 401,
+                    "success" => false,
+                    "message" => "Unauthenticated",
+                    "data" => [
+                        "message" => "Unauthenticated"
+                    ]
+                ];
+                return response()->json($response, 401);
+            }
+        });
+
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            $errors = [];
+            $firstErrorMessage = null;
+
+            foreach ($e->errors() as $field => $messages) {
+                if ($firstErrorMessage === null) {
+                    // Set the first error message
+                    $firstErrorMessage = reset($messages);
+                }
+
+                // Add error message for each field
+                $errors[$field] = [
+                    'code' => $field,
+                    'message' => $messages[0], // Pick the first error message for the field
+                ];
+            }
+
+            // Include the first error message separately under "message" key
+            $data = ['message' => $firstErrorMessage] + $errors;
+
+            return response()->json([
+                'status' => 422,
+                'success' => false,
+                'data' => $data,
+            ], $e->status);
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'status' => 404,
+                    'success' => false,
+                    'message' => 'Record not found'
+                ], 404);
+            }
+        });
     })->create();
